30cb51fddf2ecf6d7a30b62895670ab0b6c21c4a
Sushil911 <Sushilbasyal30@gmail.com>
2024-11-23 17:28:10 +0545

    Basic Togler

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -3,11 +3,13 @@ import React, { useState } from 'react'
 
 const Toggle = () => {
     const [isOn, setisOn] = useState(false)
-    const onPresshandler = () => {setisOn(!isOn)}
+    const onPressHandler = () => {
+        setisOn(!isOn)
+    }
   return (
     <View>
-        <Text> {isOn ? 'On':'Off'} </Text>
-        <Button title='Toggle' onPress={onPresshandler} />
+      <Text> {isOn ? 'On':'Off'} </Text>
+      <Button title='Toggle' onPress={onPressHandler} />
     </View>
   )
 }
